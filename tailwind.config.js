b159fcda0019f9b4c0abb464b0b071c50cf86cc7
bcapaz <bmcapaz@gmail.com>
2026-04-12 18:06:03 -0300

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx}",
-    "./src/components/**/*.{js,ts,jsx,tsx}",
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
       colors: {
-        msn: {
-          blue: '#86b9e0',
-          dark: '#235d81',
-          light: '#eef5fb',
-          border: '#a5c3d9',
-        }
-      },
+        'msn-blue': '#86b9e0',
+      }
     },
   },
   plugins: [],
