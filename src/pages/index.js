99f7f1ba61a327b83fcabc00545fc1aad7d1314c
bcapaz <bmcapaz@gmail.com>
2026-04-12 17:37:05 -0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,7 @@ export default function Login() {
           {/* Aqui entrariam os inputs de login que faremos depois */}
           <div className="p-10 flex flex-col items-center">
              <img src="/images/default-avatar.png" className="w-24 h-24 mb-4" />
-             <input className="msn-input w-full mb-2" placeholder="email@exemplo.com" />
+             <input className="msn-input w-full mb-2" placeholder="Nome da Delegação" />
              {/* ... */}
           </div>
         </WindowFrame>
