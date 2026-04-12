1b419684cfedd14b82f7d8613382321b96606b8c
bcapaz <bmcapaz@gmail.com>
2026-04-12 19:17:43 -0300

    Update prisma.js

--- src/lib/prisma.js
+++ src/lib/prisma.js
@@ -1,15 +1,13 @@
 import { PrismaClient } from '@prisma/client';
 
-// O PrismaClient é instanciado globalmente para evitar 
-// múltiplas conexões em ambiente de desenvolvimento (Hot Reload)
+const prismaClientSingleton = () => {
+  return new PrismaClient();
+};
+
 const globalForPrisma = global;
 
-export const prisma =
-  globalForPrisma.prisma ||
-  new PrismaClient({
-    log: ['query'], // Opcional: mostra as queries no console para debugar
-  });
-
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
+const prisma = globalForPrisma.prisma ?? prismaClientSingleton();
 
 export default prisma;
+
+if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
